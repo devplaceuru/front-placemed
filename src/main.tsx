510232347d3bedfaaccf14eb9c680fe18166f5d2
Moises <moisesferreira447@gmail.com>
2024-11-21 12:11:41 -0300

    Concluindo configuração do tema

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import { ThemeProvider } from "styled-components";
+import theme from "./theme";
+
+import { Login } from "./pages/Login";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Login />
+    </ThemeProvider>
   </StrictMode>
 );
---
+++ src/theme/index.ts
@@ -0,0 +1,23 @@
+export default {
+  COLOR: {
+    GRAY_200: "#E5E7EB",
+    GRAY_300: "#d1d5db",
+    GRAY_400: "#9ca3af",
+    GRAY_500: "#6b7280",
+    GRAY_600: "#4b5563",
+    GRAY_950: "#111827",
+
+    BLUE_500: "#3b82f6",
+    BLUE_700: "#1d4ed8",
+    BLUE_800: "#1e40af",
+    BLUE_950: "#172554",
+
+    WHITE: "#FFFFFF",
+  },
+
+  FONT_SIZE: {
+    XS: 14, // EXTRA SMALL
+    SM: 16, // SMALL
+    MD: 22,
+  },
+};
