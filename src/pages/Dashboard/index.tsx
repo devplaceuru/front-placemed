ccdc11fcd5a5650f9f156b74b3a606bb2b49d5d5
Moises <moisesferreira447@gmail.com>
2024-11-28 21:51:18 -0300

    Construção da area Desktop - Tela Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,60 @@
-import { Container } from "./style";
+import {
+  Container,
+  Desktop,
+  Title,
+  ContentCards,
+  Card,
+  Icone,
+  DataCard,
+  TitleDataCard,
+  CountDataCard,
+  ContentDatas,
+  CardPatientsConsults,
+} from "./style";
 import { SideBarUser } from "../../components/SideBarUser";
+import { PiBellBold, PiCalendarBlankBold, PiUserBold } from "react-icons/pi";
 
 export function Dashboard() {
   return (
     <Container>
       <SideBarUser />
+      <Desktop>
+        <Title>Dashboard</Title>
+        <ContentCards>
+          <Card backgroundColor="#172554">
+            <Icone>
+              <PiUserBold size={50} color="#E5E7EB" />
+            </Icone>
+            <DataCard>
+              <TitleDataCard>Pré-cadastros</TitleDataCard>
+              <CountDataCard>03</CountDataCard>
+            </DataCard>
+          </Card>
+          <Card backgroundColor="#1E40AF">
+            <Icone>
+              <PiCalendarBlankBold size={50} color="#E5E7EB" />
+            </Icone>
+            <DataCard>
+              <TitleDataCard>Consultas</TitleDataCard>
+              <CountDataCard>08</CountDataCard>
+            </DataCard>
+          </Card>
+          <Card backgroundColor="#1D4ED8">
+            <Icone>
+              <PiBellBold size={50} color="#E5E7EB" />
+            </Icone>
+            <DataCard>
+              <TitleDataCard>Notificações</TitleDataCard>
+              <CountDataCard>02</CountDataCard>
+            </DataCard>
+          </Card>
+        </ContentCards>
+
+        <ContentDatas>
+          <CardPatientsConsults></CardPatientsConsults>
+          <CardPatientsConsults></CardPatientsConsults>
+        </ContentDatas>
+      </Desktop>
     </Container>
   );
 }
--- src/pages/Dashboard/style.ts
+++ src/pages/Dashboard/style.ts
@@ -1,3 +1,88 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+interface Props {
+  backgroundColor: string;
+}
+
+export const Container = styled.div`
+  display: flex;
+`;
+
+export const Desktop = styled.div`
+  width: 87%;
+  height: 100vh;
+  background-color: ${({ theme }) => theme.COLOR.GRAY_200};
+  padding: 32px 0 0 39px;
+`;
+
+export const Title = styled.span`
+  font-weight: 500;
+  font-size: 32px;
+`;
+
+export const ContentCards = styled.div`
+  width: 90%;
+  height: 108px;
+  margin-top: 33px;
+  display: flex;
+  gap: 24px;
+  border: 1px solid black;
+`;
+
+export const Card = styled.div<Props>`
+  width: 330px;
+  height: 100%;
+  border-radius: 10px;
+  background-color: ${(props) => props.backgroundColor};
+  display: flex;
+`;
+
+export const Icone = styled.div`
+  width: 78px;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 39px;
+`;
+
+export const DataCard = styled.div`
+  width: 75%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const TitleDataCard = styled.span`
+  font-size: 24px;
+  font-weight: 500;
+  color: white;
+  margin-left: 20px;
+`;
+
+export const CountDataCard = styled.div`
+  font-size: 24px;
+  font-weight: 500;
+  color: white;
+  margin-left: 20px;
+`;
+
+export const ContentDatas = styled.div`
+  border: 1px solid black;
+
+  width: 90%;
+  height: 620px;
+  margin-top: 62px;
+  display: flex;
+  gap: 17px;
+`;
+
+export const CardPatientsConsults = styled.div`
+  border: 1px solid black;
+
+  width: 100%;
+  height: 620px;
+  background-color: white;
+  border-radius: 10px;
+`;
